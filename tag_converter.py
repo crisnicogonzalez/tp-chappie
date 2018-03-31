606b8f53fe469895334f8d7496bdd1481d9502b6
crisnicogonzalez <cristian3629@gmail.com>
2018-03-31 02:50:44 -0300

    Add target converter and basic app

--- tag_converter.py
+++ tag_converter.py
@@ -1,30 +1,36 @@
+from nltk import help,download
+download('tagsets')
+
 
 def get_meaning_from_tag(tag):
     return {
-        "ADJ":"adjetive",
-        "ADV":"adverb",
-        "SNJ":"conjunction",
-        "DET":"determiner",
-        "EX":"existencial",
-        "FW":"foreign word",
-        "MOD":"modal verb",
-        "N":"noun",
-        "NP":"proper noun",
-        "NUM":"number",
-        "PRO":"pronoum",
-        "P":"preposition",
-        "TO":"the word to",
-        "UH":"interjection",
-        "V":"verb",
-        "VD":"past tense",
-        "VG":"present participle",
-        "VN":"past participle",
-        "WH":"WH determiner",
-        "NNP":"NNP",
-        "NN":"NN"
+        "ADJ": "adjetive",
+        "ADV": "adverb",
+        "SNJ": "conjunction",
+        "DET": "determiner",
+        "EX": "existencial",
+        "FW": "foreign word",
+        "MOD": "modal verb",
+        "NN": "noun, common, singular or mass",
+        "NP": "proper noun",
+        "NUM": "number",
+        "PRO": "pronoum",
+        "P": "preposition",
+        "TO": "the word to",
+        "UH": "interjection",
+        "V": "verb",
+        "VD": "past tense",
+        "VG": "present participle",
+        "VN": "past participle",
+        "WH": "WH determiner",
+        "NNP": "noun, proper, singular"
     }[tag]
 
 
+def get_meaning_from_nltk(tag):
+    help.upenn_tagset(tag)
+
 def print_tagger_result(tagger_result):
     for x in tagger_result:
-        print x[0]+' -> '+get_meaning_from_tag(x[1])
+        print x[0]
+        get_meaning_from_nltk(x[1])
